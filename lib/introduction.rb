bc94dd3c5a882383b8fc7ec9cddc19afc6ef9f6b
DAZUN XING <dazun.xing@gmail.com>
2020-03-31 08:53:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,3 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
-enddef introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction_with_language(name, language)
+  puts"Hi, my name is #{name} and I am learning to program in #{language}."
 end
